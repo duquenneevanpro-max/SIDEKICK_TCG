2e578b9cc08bcd7d17480feaa9154bc5ab885077
duquenneevanpro-max <duquenneevanpro@gmail.com>
2026-01-08 08:42:49 +0100

    produits.js

---
+++ js/produits.js
@@ -0,0 +1,24 @@
+const produits = [
+  {
+    id: 1,
+    nom: "Display Pokémon FR – Écarlate & Violet",
+    prix: 129.99,
+    stock: 5,
+    type: "stock"
+  },
+  {
+    id: 2,
+    nom: "ETB Pokémon JP – Future Flash",
+    prix: 89.99,
+    stock: 0,
+    type: "precommande",
+    sortie: "Mars 2026"
+  },
+  {
+    id: 3,
+    nom: "Booster FR – Lumière Interdite",
+    prix: 5.99,
+    stock: 12,
+    type: "stock"
+  }
+];
